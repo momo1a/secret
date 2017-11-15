5c078c42a0890364a4573aa1bbc8fb09ba5d0e49
莫世友 <momo1a@qq.com>
2017-11-15 17:53:07 +0800

    【sc】

--- tools/Spider.php
+++ tools/Spider.php
@@ -10,70 +10,162 @@ class Spider
 {
 
 
+
+
+    protected $_userAgent = array(
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36 OPR/26.0.1656.60",
+        "Opera/8.0 (Windows NT 5.1; U; en)",
+        "Mozilla/5.0 (Windows NT 5.1; U; en; rv:1.8.1) Gecko/20061208 Firefox/2.0.0 Opera 9.50",
+        "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; en) Opera 9.50",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:34.0) Gecko/20100101 Firefox/34.0",
+        "Mozilla/5.0 (X11; U; Linux x86_64; zh-CN; rv:1.9.2.10) Gecko/20100922 Ubuntu/10.10 (maverick) Firefox/3.6.10",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/534.57.2 (KHTML, like Gecko) Version/5.1.7 Safari/534.57.2",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11",
+        "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/534.16 (KHTML, like Gecko) Chrome/10.0.648.133 Safari/534.16",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.101 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.11 TaoBrowser/2.0 Safari/536.11",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.71 Safari/537.1 LBBROWSER",
+        "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E; LBBROWSER)",
+        "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E; LBBROWSER)",
+        "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E; QQBrowser/7.0.3698.400)",
+        "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E)",
+        "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.84 Safari/535.11 SE 2.X MetaSr 1.0",
+        "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; SV1; QQDownload 732; .NET4.0C; .NET4.0E; SE 2.X MetaSr 1.0)",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Maxthon/4.4.3.4000 Chrome/30.0.1599.101 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.122 UBrowser/4.0.3214.0 Safari/537.36",
+
+        /*  mobile  */
+        "Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_3_3 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8J2 Safari/6533.18.5",
+        "Mozilla/5.0 (iPod; U; CPU iPhone OS 4_3_3 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8J2 Safari/6533.18.5",
+        "Mozilla/5.0 (iPad; U; CPU OS 4_2_1 like Mac OS X; zh-cn) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8C148 Safari/6533.18.5",
+        "Mozilla/5.0 (iPad; U; CPU OS 4_3_3 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8J2 Safari/6533.18.5",
+        "Mozilla/5.0 (Linux; U; Android 2.2.1; zh-cn; HTC_Wildfire_A3333 Build/FRG83D) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1",
+        "Mozilla/5.0 (Linux; U; Android 2.3.7; en-us; Nexus One Build/FRF91) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1",
+        "MQQBrowser/26 Mozilla/5.0 (Linux; U; Android 2.3.7; zh-cn; MB200 Build/GRJ22; CyanogenMod-7) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1",
+        "Opera/9.80 (Android 2.3.4; Linux; Opera Mobi/build-1107180945; U; en-GB) Presto/2.8.149 Version/11.10",
+        "Mozilla/5.0 (Linux; U; Android 3.0; en-us; Xoom Build/HRI39) AppleWebKit/534.13 (KHTML, like Gecko) Version/4.0 Safari/534.13",
+        "Mozilla/5.0 (BlackBerry; U; BlackBerry 9800; en) AppleWebKit/534.1+ (KHTML, like Gecko) Version/6.0.0.337 Mobile Safari/534.1+",
+        "Mozilla/5.0 (hp-tablet; Linux; hpwOS/3.0.0; U; en-US) AppleWebKit/534.6 (KHTML, like Gecko) wOSBrowser/233.70 Safari/534.6 TouchPad/1.0",
+        "Mozilla/5.0 (SymbianOS/9.4; Series60/5.0 NokiaN97-1/20.0.019; Profile/MIDP-2.1 Configuration/CLDC-1.1) AppleWebKit/525 (KHTML, like Gecko) BrowserNG/7.1.18124",
+        "Mozilla/5.0 (compatible; MSIE 9.0; Windows Phone OS 7.5; Trident/5.0; IEMobile/9.0; HTC; Titan)",
+
+        "UCWEB7.0.2.37/28/999",
+        "NOKIA5700/ UCWEB7.0.2.37/28/999",
+
+        "Openwave/ UCWEB7.0.2.37/28/999",
+        "Mozilla/4.0 (compatible; MSIE 6.0; ) Opera/UCWEB7.0.2.37/28/999",
+
+    );
     protected $_urls = array(
         'stackoverflow'  =>  'https://stackoverflow.com/tags'
     );
 
-    protected $_headers = array(
-        'CLIENT-IP: 192.168.1.100',
-        'X-FORWARDED-FOR: 192.168.1.100'
-    );
 
     public function __construct()
     {
-
+        $this->_printMeg('Spider Initialization.');
     }
 
     public function parse(){
         foreach ($this->_urls as $key=>$url){
+            $this->_printMeg('Spider start fetch '.$key.'.');
             switch ($key){
                 case 'stackoverflow':
-                    $this->fetStackOverFlow($url,'https://stackoverflow.com');
+                    $this->fetchStackOverFlow($url,'https://stackoverflow.com');
                     break;
                 default :
                     break;
             }
-
         }
-
 
     }
 
-    protected function fetStackOverFlow($url,$referer){
-        $document = new DOMDocument();
-        $documentTag = $this->_httpRequest($url,$referer,$this->_headers);
-        @$document->loadHTML($documentTag);
-        $xpath = new DOMXPath($document);
+    protected function fetchStackOverFlow($url,$referer){
+
+        $documentTag = $this->_httpRequest($url,$referer);
+        $xpath = $this->_loadXpath($documentTag);
         $pageTagsNode = $xpath->query('//table[@id="tags-browser"]/tr/td/a[@class="post-tag"]');
         for ($i = 0; $i<$pageTagsNode->length; $i++){
-            $tagsListPage = $pageTagsNode->item($i)->getAttribute('href');
+            $tagsListPage = 'https://stackoverflow.com'.$pageTagsNode->item($i)->getAttribute('href');
             $tagName = $pageTagsNode->item($i)->textContent;
-            echo 'https://stackoverflow.com'.$tagsListPage."===".$tagName."\r\n";
+            $this->_printMeg('Start request '.$tagsListPage);
+            //echo 'https://stackoverflow.com'.$tagsListPage."===".$tagName."\r\n";
         }
 
     }
 
-    protected function _httpRequest($url,$referer,$header){
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_TIMEOUT, 30);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch,CURLOPT_SSL_VERIFYHOST,false);
 
-        // 模拟来源
-        curl_setopt($ch, CURLOPT_REFERER, $referer);
+    protected function _loadXpath($documentTree){
+        $document = new DOMDocument();
+        @$document->loadHTML($documentTree);
+        $xpath = new DOMXPath($document);
 
-        $response = curl_exec($ch);
+        return $xpath;
+    }
 
-        if($error=curl_error($ch)){
-            die($error);
+    /**
+     * http request
+     * @param $url
+     * @param $referer
+     * @return mixed
+     */
+    protected function _httpRequest($url,$referer){
+        //随机IP
+        $ip2id= round(rand(600000, 2550000) / 10000);
+        $ip3id= round(rand(600000, 2550000) / 10000);
+        $ip4id= round(rand(600000, 2550000) / 10000);
+        $arr_1 = array("218","218","66","66","218","218","60","60","202","204","66","66","66","59","61","60","222","221","66","59","60","60","66","218","218","62","63","64","66","66","122","211");
+        $randarr= mt_rand(0,count($arr_1)-1);
+        $ip1id = $arr_1[$randarr];
+        $ip = $ip1id.".".$ip2id.".".$ip3id.".".$ip4id;
+        try {
+            $ch2 = curl_init();
+            curl_setopt($ch2, CURLOPT_URL, $url);
+            curl_setopt($ch2, CURLOPT_TIMEOUT, 100);  // 100秒超时
+            curl_setopt($ch2, CURLOPT_HTTPHEADER, array('X-FORWARDED-FOR:' . $ip, 'CLIENT-IP:' . $ip));
+
+            //curl_setopt($ch2, CURLOPT_HEADER, true);  //追踪返回302状态码，继续抓取
+            curl_setopt($ch2, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch2, CURLOPT_FOLLOWLOCATION, true);
+
+            curl_setopt($ch2, CURLOPT_NOBODY, false);
+            curl_setopt($ch2, CURLOPT_REFERER, $referer);//模拟来路
+            curl_setopt($ch2, CURLOPT_USERAGENT, $this->_userAgent[rand(0,count($this->_userAgent) - 1)]);
+
+            // 解析gzip压缩
+            curl_setopt($ch2,CURLOPT_ENCODING,'gzip,deflate');
+
+            /* 跳过ssl错误验证s */
+            curl_setopt($ch2, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($ch2,CURLOPT_SSL_VERIFYHOST,false);
+            /* 跳过ssl错误验证e */
+
+            $temp = curl_exec($ch2);
+            if (FALSE === $temp)
+                throw new Exception(curl_error($ch2), curl_errno($ch2));
+            curl_close($ch2);
+        }catch (Exception $e){
+            $this->_printMeg(
+                sprintf('Curl failed with error #%d: %s',$e->getCode(), $e->getMessage()),
+                '500',
+                'ERROR'
+            );
         }
+        return $temp;
+    }
 
-        curl_close($ch);
+    /**
+     * print message
+     * @param string $level  INFO WARNING ERROR
+     * @param string $code
+     * @param string $msg
+     */
+    protected function _printMeg($msg = 'success',$code = '200' ,$level = 'INFO' ){
 
-        return $response;
+        echo "[".date('Y-m-d H:i:s')."]Spider:::[".$code."][".strtoupper($level)."]:::".$msg."\r\n";
+
     }
 
 }
